c32758dffbe3c74b1283172a430cc21116735100
annisadiann <annisadian0412@gmail.com>
2025-11-06 17:02:49 +0700

    tambahkan field input alamat dan jarak antar elemen

--- app/src/main/java/com/example/praktikum5/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum5/view/Formulir.kt
@@ -77,3 +77,12 @@ fun FormIsian(
                 thickness = 1.dp,
                 color = Color.Red
             )
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .width(width = 250.dp),
+                label = {Text(text = "Alamat")},
+                onValueChange = {},
+            )
+            Spacer(modifier = Modifier.height(height = 30.dp))
