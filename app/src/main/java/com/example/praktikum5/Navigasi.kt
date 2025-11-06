9c03fdf1e04eaf236e168c31b6a216560e8345bb
annisadiann <annisadian0412@gmail.com>
2025-11-06 17:34:04 +0700

    tambahkan fungsi bantu untuk navigasi kembali ke layar Formulirku

--- app/src/main/java/com/example/praktikum5/Navigasi.kt
+++ app/src/main/java/com/example/praktikum5/Navigasi.kt
@@ -47,3 +47,9 @@ fun DataApp(
     }
 }
 
+private fun cancelAndBackToFormulirku(
+    navController: NavHostController
+){
+    navController.popBackStack(route = Navigasi.Formulirku.name,
+        inclusive = false)
+}
