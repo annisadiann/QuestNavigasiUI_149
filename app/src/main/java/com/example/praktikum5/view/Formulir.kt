f1f23c865e0a59f67548f0efb444228097e79c08
annisadiann <annisadian0412@gmail.com>
2025-11-06 16:57:29 +0700

    tambahkan Column utama untuk menampung elemen form

--- app/src/main/java/com/example/praktikum5/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum5/view/Formulir.kt
@@ -42,3 +42,7 @@ fun FormIsian(
             )
         }
     ){ isiRuang ->
+        Column(modifier = Modifier.padding(paddingValues=isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
