a9cbb868863a0488c691048f02149c7a95ae962e
annisadiann <annisadian0412@gmail.com>
2025-11-06 17:01:18 +0700

    tambahkan pilihan jenis kelamin menggunakan RadioButton

--- app/src/main/java/com/example/praktikum5/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum5/view/Formulir.kt
@@ -59,3 +59,15 @@ fun FormIsian(
                 .padding(all = 20.dp)
                 .width(width = 250.dp), thickness = 1.dp,color =
                 Color.Red)
+            Row{
+                jenisk.forEach {
+                        item->
+                    Row(verticalAlignment = Alignment.CenterVertically){
+                        RadioButton(
+                            selected = false,
+                            onClick = {item}
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
