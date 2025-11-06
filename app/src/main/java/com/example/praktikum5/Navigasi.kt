d78022798eaf4944e61fa5208b58da64488ef34f
annisadiann <annisadian0412@gmail.com>
2025-11-06 17:33:43 +0700

    tambahkan NavHost dengan dua rute — FormIsian dan TampilData

--- app/src/main/java/com/example/praktikum5/Navigasi.kt
+++ app/src/main/java/com/example/praktikum5/Navigasi.kt
@@ -22,3 +22,28 @@ fun DataApp(
     modifier: Modifier
 ){
     Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding(paddingValues = isiRuang)
+        ){
+            composable(route = Navigasi.Formulirku.name){
+                FormIsian(
+                    //pilihanJK = JenisK.map {id -> konteks.resources.getString(id) }
+                    OnSubmitBtnClick = {
+                        navController.navigate(route = Navigasi.Detail.name)
+                    })
+            }
+            composable(route = Navigasi.Detail.name){
+                TampilData(
+                    onBackBtnClick = {
+                        cancelAndBackToFormulirku(navController)
+                    }
+                )
+
+            }
+        }
+    }
+}
+
